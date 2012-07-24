20175c135a4b48a5d30f1419b562ae606aef52e8
Benoit Girard <b56girard@gmail.com>
2012-07-23 21:00:44 -0400

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh bgirard@people.mozilla.org "cd public_html/Invalidation && echo Pull \$PWD && git pull && chmod 755 platformdebug.*"
+ssh bgirard@people.mozilla.org "cd public_html/Invalidation && echo Pull \$PWD && git pull"
